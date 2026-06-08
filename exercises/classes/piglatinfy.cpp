d9d4001e4bd87dd57ca3307e62ed8b41c049491b
magnus <magnuspicataggio@gmail.com>
2026-06-07 21:46:16 -0400

    Finished the p-set

--- exercises/classes/piglatinfy.cpp
+++ exercises/classes/piglatinfy.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
+#include <string>
+std::string piglatinify(const std::string& word) {
+    const std::string vowels = "aeiou";
 
-std::string piglatinfy(const std::string string) {
-    std::string latinfied = "";
+    auto is_vowel = [&vowels](char c) {
+        return vowels.find(c) != std::string::npos;
+    };
 
-    for (const char& ch : string) {
+    if (word.empty()) return word;
+
+    if (is_vowel(word[0])) {
+        return word + "way";
     }
 
-    return latinfied;
+    // treat 'qu' as a single consonant
+    if (word[0] == 'q' && word.size() > 1 && word[1] == 'u') {
+        return word.substr(2) + "-quay";
+    }
+
+    return word.substr(1) + "-" + word[0] + "ay";
 }
